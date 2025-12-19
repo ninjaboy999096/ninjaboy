258c1fc291655fe7e8b3cf31e173dc8c073bc6af
ninjaboy999096 <129236203+ninjaboy999096@users.noreply.github.com>
2025-12-18 19:23:49 -0800

    Add files via upload

---
+++ rugplaylib/dropdown-menu/dropdown-menu-trigger.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui";
+	import { browser } from "$app/environment";
+
+	let {
+		ref = $bindable(null),
+		...restProps
+	}: DropdownMenuPrimitive.TriggerProps = $props();
+</script>
+
+{#if browser}
+	<DropdownMenuPrimitive.Trigger
+		bind:ref
+		data-slot="dropdown-menu-trigger"
+		{...restProps}
+	>
+		{@render restProps.children?.()}
+	</DropdownMenuPrimitive.Trigger>
+{:else}
+	<!-- SSR fallback -->
+	<button
+		bind:this={ref}
+		data-slot="dropdown-menu-trigger"
+		type="button"
+		{...restProps}
+	>
+		{@render restProps.children?.()}
+	</button>
+{/if}
